9debb9d2d21342d38032ba0f85efe3a806b64f48
Christian Quest <cquest@openstreetmap.fr>
2018-06-15 16:39:43 +0200

    complément nom des champs et contraintes

--- package.py
+++ package.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from datapackage import Package
 
 pkg = Package()
@@ -6,9 +8,34 @@ pkg.infer('2015.csv')
 pkg.infer('2016.csv')
 pkg.infer('2017.csv')
 for r in range(0,len(pkg.descriptor['resources'])):
+    pkg.descriptor['resources'][r]['schema']['fields'][0]['title'] = 'Code INSEE de la commune'
     pkg.descriptor['resources'][r]['schema']['fields'][0]['type'] = 'string'
-    for c in range(3,17):
-        pkg.descriptor['resources'][r]['schema']['fields'][c-1]['type'] = 'integer'
+    pkg.descriptor['resources'][r]['schema']['fields'][0]['constraints'] = [dict(required=True,
+                                                                                 unique=True,
+                                                                                 pattern='[0-9][0-9A-B][0-9]{3}')
+                                                                           ]
+    pkg.descriptor['resources'][r]['schema']['fields'][1]['title'] = 'Nom de la commune'
+    pkg.descriptor['resources'][r]['schema']['fields'][1]['constraints'] = [dict(required=True,
+                                                                                 maxLength=45,
+                                                                                 minLength=1)]
+
+    pkg.descriptor['resources'][r]['schema']['fields'][2]['title'] = 'dotation forfaitaire (DF)'
+    pkg.descriptor['resources'][r]['schema']['fields'][3]['title'] = 'dotation élu local (DPEL)'
+    pkg.descriptor['resources'][r]['schema']['fields'][4]['title'] = 'dotation de solidarité urbaine et de cohésion sociale (DSU)'
+    pkg.descriptor['resources'][r]['schema']['fields'][5]['title'] = 'dotation de solidarité rurale "bourg centre" (DSR BC)'
+    pkg.descriptor['resources'][r]['schema']['fields'][6]['title'] = 'dotation de solidarité rurale "péréquation" (DSR P)'
+    pkg.descriptor['resources'][r]['schema']['fields'][7]['title'] = 'dotation nationale de péréquation (DNP)'
+    pkg.descriptor['resources'][r]['schema']['fields'][8]['title'] = "dotation d'aménagement des communes d'outre-mer (DACOM)'"
+    pkg.descriptor['resources'][r]['schema']['fields'][9]['title'] = 'dotation de solidarité rurale "cible" (DSR C)'
+    pkg.descriptor['resources'][r]['schema']['fields'][10]['title'] = 'Prélèvement de la commune isolée'
+    pkg.descriptor['resources'][r]['schema']['fields'][11]['title'] = 'Versement FPIC au profit de la commune isolée'
+    pkg.descriptor['resources'][r]['schema']['fields'][12]['title'] = 'Solde FPIC de la commune isolée'
+    pkg.descriptor['resources'][r]['schema']['fields'][13]['title'] = 'Prélèvement FSRIF de la commune'
+    pkg.descriptor['resources'][r]['schema']['fields'][14]['title'] = 'Versement FSRIF de la commune'
+    pkg.descriptor['resources'][r]['schema']['fields'][15]['title'] = 'Solde FSRIF de la commune'
+
+    for c in range(2,16):
+        pkg.descriptor['resources'][r]['schema']['fields'][c]['type'] = 'integer'
 
 pkg.descriptor['name'] = 'Dotations Globales de Fonctionnement'
 pkg.descriptor['homepage'] = 'http://www.dotations-dgcl.interieur.gouv.fr/'
@@ -26,7 +53,4 @@ pkg.descriptor['contributors'] = [dict(title='Christian Quest',
 
 pkg.descriptor['temporal'] = dict(start='2014-01-01', end='2017-12-31')
 pkg.commit()
-if pkg.valid:
-    pkg.save('datapackage.json')
-else:
-    print('error')
+pkg.save('datapackage.json')
